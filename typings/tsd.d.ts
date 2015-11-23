e8073e03e7ac66d56a2d9a6ab644025e87e9d926
Thales Mello <thalesmello@gmail.com>
2015-11-23 01:36:26 -0200

    including typescript definition for used libraries

---
+++ typings/tsd.d.ts
@@ -0,0 +1,5 @@
+/// <reference path="jasmine/jasmine.d.ts" />
+/// <reference path="gulp/gulp.d.ts" />
+/// <reference path="node/node.d.ts" />
+/// <reference path="orchestrator/orchestrator.d.ts" />
+/// <reference path="q/Q.d.ts" />
