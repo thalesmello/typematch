2744f3b0051eac06b0522b9583ff706019ffd272
Thales Mello <thalesmello@gmail.com>
2015-11-24 02:37:39 -0200

    library now supports two arguments

--- spec/typematch_spec.ts
+++ spec/typematch_spec.ts
@@ -1,29 +1,48 @@
 /// <reference path="../typings/jasmine/jasmine.d.ts" />
 
-import {match, _} from '../src/typematch'
+import * as tm from '../src/typematch'
+const {match1, _} = tm;
 
 describe('the TypeMatch module', () => {
 	it('exists', () => {
-		expect(match).not.toBeNull();
+		expect(match1).not.toBeNull();
 	});
 	
 	it('makes a simple assertion', () => {
-		let result = match('it is',
+		let result = match1('it is',
 			['it is', () => 'correct']);
 		expect(result).toEqual('correct');
 	});
 	
 	it('accepts multiple assertions', () => {
-		let result = match(1,
+		let result = match1(1,
 			[0, () => 'nope'],
 			[1, () => 'yes']);
 		expect(result).toEqual('yes');
 	});
 	
-	it('it has wildcarts', () => {
-		let result = match('yolo',
+	it('has wildcarts', () => {
+		let result = match1('yolo',
 			['hi', () => 'there'],
 			[_, () => 'good bye']);
 		expect(result).toEqual('good bye');
 	});
+	
+	it('can take arguments in conditions', () => {		
+		function fibonacci(n : number): number {
+			return match1(n, 
+				[0, () => 1],
+				[_, (n) => n * fibonacci(n - 1)])
+		}
+		
+		expect(fibonacci(5)).toEqual(120)
+	});
+})
+
+describe('module with variadic args', () => {
+	it('accepts two arguments', () => {
+		let result = tm.match2('foo', 'bar',
+			['foo', _, () => 'yes!']);
+		expect(result).toEqual('yes!');
+	})
 })
--- src/typematch.ts
+++ src/typematch.ts
@@ -1,12 +1,74 @@
-export interface Wildcard {}
+export class Wildcard {
+	private identity;
+	constructor() {
+		this.identity = {};
+	}
+	
+	equals(other: Wildcard) {
+		return this.identity === other.identity;
+	}
+}
 
-export const _ : Wildcard = {}
+export const _ = new Wildcard();
 
-export function match<TValue, TResponse>(value: TValue, ...conditions: [TValue | Wildcard, () => TResponse][]){
-	for(let [check, returnFunc] of conditions) {
-		console.log({value: value, check: check})
-		if(value === check || check === _) {
-			return returnFunc();
+export interface Func<TArgument, TResponse> {
+	(TArgument?): TResponse
+}
+
+function checkEquals<TValue>(values: TValue[], checks: (TValue|Wildcard)[]): boolean {
+	for(let i = 0; i < values.length; i++) {
+		let value = values[i];
+		let check = checks[i];
+		if(!(check instanceof Wildcard && _.equals(check) || value === check)) {
+			return false;
 		}
 	}
+	return true;
+}
+
+export type Multi<TValue> = TValue|TValue[]
+
+function toList<TValue>(values: Multi<TValue>): TValue[] {
+	if (values instanceof Array) {
+		return values;
+	}
+	else {
+		return [<TValue>values]
+	}
+} 
+
+export type Check<TValue> = TValue|Wildcard
+
+export function match<T1, TResp>(value: T1, ...conditions: [Check<T1>, (TValue?) => TResp][]){
+	for(let [check, returnFunc] of conditions) {
+		let checks = toList(check);
+		if(checkEquals([value], checks)) {
+			return returnFunc(value);
+		}
+	}
+}
+
+export function matchAny<TVal, TResp>(values: TVal[], ...conditions: [Check<TVal>[], (...vals: TVal[]) => TResp][]): TResp{
+	for(let [checks, returnFunc] of conditions) {
+		if(checkEquals(values, checks)) {
+			return returnFunc(...values);
+		}
+	}
+}
+
+export type AnyFunc = (...args: any[]) => any
+export type MyFunc<TVal, TResp> = (...args: TVal[]) => TResp
+
+function extractChecks<TVal, TResp>(args: (Check<TVal>|MyFunc<TVal, TResp>)[]): [Check<TVal>[], MyFunc<TVal, TResp>]{
+	var checks = <Check<TVal>[]> args.slice(0, args.length - 1);
+	var func = <MyFunc<TVal, TResp>> args[args.length - 1]; 
+	return [checks, func];
+}
+
+export function match1<T1, TResp>(v1: T1, ...conditions: [Check<T1>, (v1?: T1) => TResp][]){
+	return matchAny([v1], ...conditions.map((checks) => extractChecks(checks)))
+};
+
+export function match2<T1, T2, TResp>(v1: T1, v2: T2, ...conditions: [Check<T1>, Check<T2>, (T1?, T2?) => TResp][]){
+	return matchAny([v1, v2], ...conditions.map((checks) => extractChecks(checks)))
 }
