3f4d195588bf51cea1c15b29ab09edbdd06e4b15
Thales Mello <thalesmello@gmail.com>
2015-11-26 12:16:40 -0200

    including task to bundle definitions

--- gulpfile.js
+++ gulpfile.js
@@ -17,12 +17,28 @@ var buildProject = ts.createProject(tsOpts);
 gulp.task('build', () => {
 	var tsStream = gulp.src('src/**/*.ts')
 		.pipe(ts(buildProject));
-		
+
 	return merge([
 		tsStream.dts.pipe(gulp.dest('bin/src')),
 		tsStream.js.pipe(gulp.dest('bin/src'))
 	]);
 });
+
+gulp.task('bundle_definitions', (done) => {
+	var opts = {
+		name: 'typematch',
+		main: 'bin/src/typematch.d.ts',
+		baseDir: 'bin',
+		out: 'src/bundle.d.ts',
+	};
+
+	// require module
+	var dts = require('dts-bundle');
+
+	// run it
+	dts.bundle(opts);
+	return done();
+})
 
 gulp.task('test', ['build', 'build_test'], () => {
 	return gulp.src('bin/spec/*_spec.js')
